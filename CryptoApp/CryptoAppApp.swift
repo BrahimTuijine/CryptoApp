0979bc4932c6f4a56896b3c84f23b89e62f9c3be
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-11-07 10:27:37 +0100

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by MacBook on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
