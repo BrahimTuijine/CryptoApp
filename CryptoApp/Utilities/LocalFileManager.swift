a3ab7dc13def8045b782a874140965d1e889c99f
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-11-10 00:36:13 +0100

    create file LocalFileManager to get and save image

---
+++ CryptoApp/Utilities/LocalFileManager.swift
@@ -0,0 +1,79 @@
+//
+//  LocalFileManager.swift
+//  CryptoApp
+//
+//  Created by MacBook on 09/11/2024.
+//
+
+import Foundation
+import SwiftUI
+
+
+protocol MyFileManager {
+    
+    var folderName: String { get set }
+    
+    func createFolderIfNeeded() -> Void
+    
+    func getLocalImage(imageName: String) -> UIImage?
+    
+    func saveLocalImage(image: UIImage, imageName: String) -> Void
+    
+    func getURLForFolder() -> URL?
+}
+
+class LocalFileManager : MyFileManager {
+    
+    static let instance = LocalFileManager()
+    
+    private init() {}
+    
+    var folderName: String = "cryptoApp"
+    
+    func createFolderIfNeeded() {
+        guard let url = getURLForFolder() else { return }
+        
+        let fileManager = FileManager.default
+        
+        if !fileManager.fileExists(atPath: url.path()) {
+            do {
+                try fileManager.createDirectory(at: url, withIntermediateDirectories: true)
+            } catch {
+                print("Error creating directory. FolderName: \(folderName). \(error)")
+            }
+        }
+    }
+    
+    func getLocalImage(imageName: String) -> UIImage? {
+        guard let url = getURLForFolder()?.appendingPathComponent(imageName),
+              FileManager.default.fileExists(atPath: url.path())
+        else { return nil }
+        
+        return UIImage(contentsOfFile: url.path())
+    }
+    
+    func saveLocalImage(image: UIImage, imageName: String) {
+        
+        // create folder
+        createFolderIfNeeded()
+        
+        // get path for image
+        guard let image = image.pngData(),
+        let url = getURLForFolder()?.appendingPathComponent(imageName)
+        else { return }
+        
+        // save image to path
+        do {
+            try image.write(to: url)
+        } catch {
+            print("Error saving image, ImageName: \(imageName) -> \(error.localizedDescription)")
+        }
+    }
+    
+    internal func getURLForFolder() -> URL? {
+        guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else {return nil}
+        
+        // add folder name for my app -> path/"cryptoApp"
+        return url.appendingPathComponent(folderName)
+    }
+}
