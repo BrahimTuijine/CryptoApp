9a99eb1eff931f2ce7a46ffff8df90ee5da0f736
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-11-13 16:06:13 +0100

    create CoinDetailsService

--- CryptoApp/Services/CoinDetailsService.swift
+++ CryptoApp/Services/CoinDetailsService.swift
@@ -6,9 +6,34 @@
 //
 
 import Foundation
-
+import Combine
 
 class CoinDetailsService: ObservableObject {
     
+    @Published var coinsDetails: CoinDetailsModel?
     
+    var cancellable: AnyCancellable?
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinsDetails()
+    }
+    
+    private func getCoinsDetails() -> Void {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/\(coin.id)?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false")
+        else { return }
+        
+        cancellable = NetworkingManager.download(url: url)
+            .decode(type: CoinDetailsModel.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion,
+                  receiveValue: { [weak self] coinDetails in
+                guard let self = self else { return }
+                self.coinsDetails = coinDetails
+                self.cancellable?.cancel()
+            })
+            
+            
+    }
 }
